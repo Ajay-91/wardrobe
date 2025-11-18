db32272f5bdd3d138e055a197e293ebd2987aa78
Ajay-91 <ajay0m911@gmail.com>
2025-11-18 14:48:02 +0530

    upp

--- src/services/allAPI.js
+++ src/services/allAPI.js
@@ -1,21 +1,22 @@
 import axiosConfig from "./axiosConfig";
+import { baseURL } from "./baseURL";
 
 export const createWar = async (reqBody) => {
-  return await axiosConfig("post", "http://localhost:3000/war", reqBody);
+  return await axiosConfig("post", `${baseURL}/war`, reqBody);
 };
 
 export const getWar = async () => {
-  return await axiosConfig("get", "http://localhost:3000/war", "");
+  return await axiosConfig("get", `${baseURL}/war`, "");
 };
 
 export const getOneWar = async (id) => {
-  return await axiosConfig("get", `http://localhost:3000/war/${id}`, "");
+  return await axiosConfig("get", `${baseURL}/war${id}`, "");
 };
 
 export const deleteWar = async (id) => {
-  return await axiosConfig("delete", `http://localhost:3000/war/${id}`, "");
+  return await axiosConfig("delete", `${baseURL}/war/${id}`, "");
 };
 
 export const updateWar = async (id, reqBody) => {
-  return await axiosConfig("patch", `http://localhost:3000/war/${id}`, reqBody);
+  return await axiosConfig("patch", `${baseURL}/war/${id}`, reqBody);
 };
