84355f9a082e72270945b48ac3fcba3de202119d
Ajay-91 <ajay0m911@gmail.com>
2025-11-24 19:58:22 +0530

    p

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Footer = () => {
   return (
     <div style={{width:"100%",marginTop:100}}>
-      <div className="d-flex flex-column bg-primary text-center text-white p-4" sx={{ width: "100%" }}>made using React and Material UI by Ajay</div>
+      <div className="d-flex flex-column bg-primary text-center text-white p-4" sx={{ width: "100%" }}>© 2025 — All rights reserved. Keeping your wardrobe organized with an easy dashboard to manage every outfit and clothing item in one place.</div>
     </div>
   )
 }
